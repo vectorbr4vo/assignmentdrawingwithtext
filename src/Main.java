f2fb1829e8765ddc232b5d6cbe2e26746e0fd294
vectorbr4vo <55299520+vectorbr4vo@users.noreply.github.com>
2019-10-18 14:50:24 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String args[]) {
+        //System.out.println("Index\tValue\n0\t\t1\n1\t\t2");
+        //System.out.println("https://i.pinimg.com/236x/4a/76/72/4a76729aad73157f7dedcda1e8ef53f9--template-minecraft-minecraft-bag.jpg");
+        System.out.println(" __________________________________");
+        System.out.println("[\t\t\t\t\t\t\t\t   ]");
+        System.out.println("|\t\t\t\t\t\t\t\t   |");
+        System.out.println("|\t\t______________________\t   |");
+        System.out.println("|\t  |\t\t\t\t\t\t |\t   |");
+        System.out.println("|_____|\t\t\t\t\t\t |_____|");
+        System.out.println("|\t  _______\t\t   _______\t   |");
+        System.out.println("|\t |_______|\t_____ |______|\t   |");
+        System.out.println("|\t\t\t  |_______|\t\t\t   |");
+        System.out.println("|\t\t\t\t\t\t\t\t   |");
+        System.out.println("|\t\t\t\t\t\t\t\t   |");
+        System.out.println("|\t\t\t|-----------|\t\t   |");
+        System.out.println("|\t\t\t[-----------]\t\t   |");
+        System.out.println("|\t\t\t\t\t\t\t\t   |");
+        System.out.println("L----------------------------------|");
+    }
+
+}
